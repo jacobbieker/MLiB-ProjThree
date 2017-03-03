05d3bfa4717f5a4dbd53de87eeafda079e496ae7
Jacob Bieker <jacob@bieker.tech>
2017-03-03 17:37:37 +0100

    Sort of done, decoding seems to be off
    Right now, only strings of "o" being printed, so not sure why. Looking into that

--- three_state.py
+++ three_state.py
@@ -17,22 +17,23 @@ sequence_annotations = []
 sequence_names = []
 spot = 1
 data_file_num = 0
-with open(os.path.join("data", "set160." + str(data_file_num) + ".labels.txt")) as sequence_data:
-    seq_reader = csv.reader(sequence_data, delimiter=" ")
-    for line in sequence_data:
-        if ">" in line:
-            sequence_names.append(line.split(">")[1].strip())
-            spot = 1
-        elif spot == 1:
-            sequences.append(line.strip())
-            spot = 0
-        elif "#" in line:
-            sequence_annotations.append(line.split("#")[1].strip())
-    data_file_num += 1
+for data_file_num in range(0, 9):
+    with open(os.path.join("data", "set160." + str(data_file_num) + ".labels.txt")) as sequence_data:
+        seq_reader = csv.reader(sequence_data, delimiter=" ")
+        for line in sequence_data:
+            if ">" in line:
+                sequence_names.append(line.split(">")[1].strip())
+                spot = 1
+            elif spot == 1:
+                sequences.append(line.strip())
+                spot = 0
+            elif "#" in line:
+                sequence_annotations.append(line.split("#")[1].strip())
 
 print(sequence_names)
 print(sequences)
 print(sequence_annotations)
+print(len(sequence_names))
 
 ##################### FINISH READING IN DATA ###################################
 ##################### CALCUATE PROBABLITIES ####################################
@@ -53,22 +54,31 @@ for index, seq in enumerate(sequences):
     for j, amino_acid in enumerate(sequence_index):
         if j == 0:
             # First one, so count in pi_table
-            pi_table[amino_acid] += 1
+            pi_table[annotation[j]] += 1
         emissions_table[annotation[j]][amino_acid] += 1
         if j > 0:
             # Can get a transition from past value to current one
             transitions_table[annotation[j-1]][annotation[j]] += 1
 
+# Now normalize all the values to 1, since everything is read in
+# Get sums
+total_pi = np.sum(pi_table)
+total_transition = np.sum(transitions_table)
+total_emissions = np.sum(emissions_table)
+
+# Now divide all values by the total
+pi_table /= total_pi
+transitions_table /= total_transition
+emissions_table /= total_emissions
+
 
 ##################### END CALCULATE PROB #######################################
 
-decoding = False
-
-if decoding:
+def viterbi_decoding(vit_sequences, vit_transitions_table, vit_emissions_table, vit_pi_table, vit_sequence_names):
     # Viterbi decoding, taken from project 2
-    for index, item in enumerate(sequences):
+    for index, item in enumerate(vit_sequences):
         seq1 = item
-        print("The test sequence: " + str(sequence_names[index]))
+        print("The test sequence: " + str(vit_sequence_names[index]))
         print(seq1)
 
         # remapping of sequence to a list of numbers, so it can be easily
@@ -77,22 +87,22 @@ if decoding:
 
         # Creation of the ω table:
         # Setting all the values to -inf for if the value is 0 in log space, should be -inf
-        omega_table = [len(pi_table) * [float("-inf")] for index in range(len(sequence_index))]
+        omega_table = [len(vit_pi_table) * [float("-inf")] for index in range(len(sequence_index))]
 
         # Calculation of the 1st column of the ω table , which is the basis for the recursion of the algorithm:
-        for j in range(len(pi_table)):
-            omega_table[0][j] = np.log(pi_table[j]) + np.log(emissions_table[j][sequence_index[0]])
+        for j in range(len(vit_pi_table)):
+            omega_table[0][j] = np.log(vit_pi_table[j]) + np.log(vit_emissions_table[j][sequence_index[0]])
 
         # Calculation of the rest of the ω table:
         for n in range(1,len(sequence_index)):
             for k in range(len(states_to_index)):
                 transition_value = float("-inf")
-                if emissions_table[k][sequence_index[n]]!=0.0 and emissions_table[k][sequence_index[n]] != float("-inf"):
+                if vit_emissions_table[k][sequence_index[n]]!=0.0 and vit_emissions_table[k][sequence_index[n]] != float("-inf"):
                     for j in range(len(states_to_index)):
-                        if transitions_table[j][k]!=0.0 and transitions_table[j][k] != float("-inf") :
-                            if omega_table[n-1][j] + np.log(transitions_table[j][k]) > transition_value:
-                                transition_value = omega_table[n-1][j] + np.log(transitions_table[j][k])
-                    omega_table[n][k] = np.log(emissions_table[k][sequence_index[n]]) + transition_value
+                        if vit_transitions_table[j][k]!=0.0 and vit_transitions_table[j][k] != float("-inf") :
+                            if omega_table[n-1][j] + np.log(vit_transitions_table[j][k]) > transition_value:
+                                transition_value = omega_table[n-1][j] + np.log(vit_transitions_table[j][k])
+                    omega_table[n][k] = np.log(vit_emissions_table[k][sequence_index[n]]) + transition_value
 
         # Creation of the Z* sequence as a list, and calculation of its last element
         Z = []
@@ -112,11 +122,36 @@ if decoding:
         # Calculation of the rest of the Z sequence backwards, from the right to the left
         for n in range(len(Z)-1,0,-1):
             for k in range(len(states_to_index)):
-                if transitions_table[k][Z[n]] != 0.0:
-                    if emissions_table[Z[n]][sequence_index[n]] != 0.0:
-                        if omega_table[n-1][k] + np.log(transitions_table[k][Z[n]]) + np.log(emissions_table[Z[n]][sequence_index[n]]) == omega_table[n][Z[n]]:
+                if vit_transitions_table[k][Z[n]] != 0.0:
+                    if vit_emissions_table[Z[n]][sequence_index[n]] != 0.0:
+                        if omega_table[n-1][k] + np.log(vit_transitions_table[k][Z[n]]) + np.log(vit_emissions_table[Z[n]][sequence_index[n]]) == omega_table[n][Z[n]]:
                             Z[n-1] = k
         print("The Z* sequence of the hidden states:\n")
         print("".join([index_to_states[c] for c in Z])) # the Z* sequence printed as a string
         print("Log value:" + str(omega_table[-1][Z[-1]]))
         print("Finished")
+
+
+decoding = True
+test_file_num = 9
+test_sequences = []
+test_sequence_annotations = []
+test_sequence_names = []
+with open(os.path.join("data", "set160." + str(test_file_num) + ".labels.txt")) as sequence_data:
+    for line in sequence_data:
+        if ">" in line:
+            test_sequence_names.append(line.split(">")[1].strip())
+            spot = 1
+        elif spot == 1:
+            test_sequences.append(line.strip())
+            spot = 0
+        elif "#" in line:
+            test_sequence_annotations.append(line.split("#")[1].strip())
+
+print(sequence_names)
+print(sequences)
+print(sequence_annotations)
+print(len(sequence_names))
+
+viterbi_decoding(test_sequences, transitions_table, emissions_table, pi_table, test_sequence_names)
+
